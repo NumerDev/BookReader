89bfec4ff4ae1f5c500573ba5d6eb52f98d92a4a
Mateusz Grzomba <numerpriv@gmail.com>
2022-06-08 18:11:08 +0200

    Fixed import path of theme

--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -1,6 +1,6 @@
 import { GlobalStyle } from "../styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import { theme } from "../styles/theme";
+import { theme } from "../styles/Theme";
 import {
   BrowserRouter as Router,
   Routes as Switch,
