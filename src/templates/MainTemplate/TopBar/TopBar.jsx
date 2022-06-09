8eba456f211d79a13cf075a8f4ffdac8995ef988
Mateusz Grzomba <numerpriv@gmail.com>
2022-06-09 23:58:00 +0200

    Temporary search functionality

--- src/templates/MainTemplate/TopBar/TopBar.jsx
+++ src/templates/MainTemplate/TopBar/TopBar.jsx
@@ -1,10 +1,30 @@
 import { MenuLink, SearchBar, TopBarWrapper } from './TopBar.styles';
 import GithubIcon from '../../../assets/GithubIcon.svg';
+import { useRef } from 'react';
+import { useEffect, useState } from 'react';
+import debounce from 'lodash.debounce';
+import { useFetchBooks } from '../../../hooks/useFetchBooks';
 
 const TopBar = () => {
+  const searchBar = useRef('');
+  const [fetchBooksData, fetchCertainBook, findBooks] = useFetchBooks();
+  const [test, setTest] = useState('');
+
+  useEffect(() => {
+    debounce(async () => {
+      const bookData = await findBooks(test);
+      console.log(test, bookData);
+    }, 500)();
+  }, [test]);
+
+  const handleChange = debounce(async (e) => {
+    searchBar.current.value = e.target.value;
+    setTest(e.target.value);
+  }, 500);
+
   return (
     <TopBarWrapper>
-      <SearchBar></SearchBar>
+      <SearchBar ref={searchBar} placeholder="Search..." onChange={(e) => handleChange(e)}></SearchBar>
       <MenuLink to="/">
         <img src={GithubIcon} alt="" />
       </MenuLink>
