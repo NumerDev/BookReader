7d67363ce9044fb24783ce017771ce8392b0a64f
Mateusz Grzomba <numerpriv@gmail.com>
2022-06-09 10:25:12 +0200

    Added github icon asset

--- src/templates/MainTemplate/TopBar/TopBar.jsx
+++ src/templates/MainTemplate/TopBar/TopBar.jsx
@@ -1,10 +1,13 @@
 import { MenuLink, SearchBar, TopBarWrapper } from './TopBar.styles';
+import GithubIcon from '../../../assets/GithubIcon.svg';
 
 const TopBar = () => {
   return (
     <TopBarWrapper>
       <SearchBar></SearchBar>
-      <MenuLink to="/">GH</MenuLink>
+      <MenuLink to="/">
+        <img src={GithubIcon} alt="" />
+      </MenuLink>
     </TopBarWrapper>
   );
 };
