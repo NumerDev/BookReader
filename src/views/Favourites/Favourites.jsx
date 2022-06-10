d9a72456977c8b53c4e599aff3a6e2c358c57c05
Mateusz Grzomba <numerpriv@gmail.com>
2022-06-10 14:42:09 +0200

    Added generating favourite books from localStorage

--- src/views/Favourites/Favourites.jsx
+++ src/views/Favourites/Favourites.jsx
@@ -1,18 +1,34 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import BookItem from '../../components/BookItem/BookItem';
 import { useFavourite } from '../../hooks/useFavourite';
-import { ContentGrid, ContentTitle } from '../BookList/BookList.styles';
+import { ContentGrid, ContentTitle, ContentMenu, ContentMenuButtonWrapper, ContentMenuButton } from '../BookList/BookList.styles';
 
 const Favourites = () => {
-  const [addFavourite, getFabourite] = useFavourite();
+  const [addFavourite, getFavourite] = useFavourite();
+  const [favBooks, setFavBooks] = useState([]);
 
+  useEffect(() => {
+    const x = getFavourite();
+    setFavBooks(getFavourite());
+  }, []);
   return (
     <>
-      <ContentTitle>Favourites</ContentTitle>
-      <ContentGrid>
-        {getFabourite().map((e) => (
-          <BookItem key={e.title} book={e} />
-        ))}
-      </ContentGrid>
+      {/* {getFavourite() && getFavourite().map((book, idx) => <BookItem key={idx} book={book} />)} */}
+      <ContentMenu>
+        <ContentTitle>Favourites</ContentTitle>
+        <ContentMenuButtonWrapper>
+          <ContentMenuButton
+            onClick={() => {
+              window.localStorage.clear();
+              setFavBooks([]);
+            }}
+          >
+            Clear All
+          </ContentMenuButton>
+        </ContentMenuButtonWrapper>
+      </ContentMenu>
+      <ContentGrid>{favBooks && favBooks.length > 0 ? favBooks.map((book, idx) => <BookItem key={idx} book={book} />) : null}</ContentGrid>
     </>
   );
 };
