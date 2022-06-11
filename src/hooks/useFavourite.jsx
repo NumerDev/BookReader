299e41dfedd3dd7b6ba00411a2bb4d5ece2d9162
Numer <numerpriv@gmail.com>
2022-06-11 10:33:19 +0200

    Deleted console logs

--- src/hooks/useFavourite.jsx
+++ src/hooks/useFavourite.jsx
@@ -3,15 +3,12 @@ import { useState, useEffect, useCallback } from 'react';
 export const useFavourite = () => {
   const addFavourite = useCallback(async (book) => {
     if (book.title == undefined) return; //Kinda workaround and i should fix that
-    console.log(book);
     const ls = window.localStorage;
     let actualFav = JSON.parse(ls.getItem('fav'));
     actualFav = actualFav ? [...actualFav] : [];
     if (actualFav.find((e) => e.title === book.title)) return;
     actualFav.push(book);
     window.localStorage.setItem('fav', JSON.stringify(actualFav));
-    console.log('parsed', JSON.parse(ls.getItem('fav')));
-    console.log(window.localStorage);
   }, []);
 
   const getFavourite = useCallback(() => {
